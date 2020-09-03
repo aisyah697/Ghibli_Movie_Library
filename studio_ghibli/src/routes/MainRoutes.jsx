ede753a4435290f0a10f297d94375f729a6db0a2
aisyahpu <aisyah.it@cls-indo.com>
2020-09-03 12:55:01 +0700

    update

--- studio_ghibli/src/routes/MainRoutes.jsx
+++ studio_ghibli/src/routes/MainRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 // import {Provider} from "react-redux"
 
 import Home from "../pages/Home";
+import Drawer from "../pages/Drawer";
 
 const MainRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const MainRoutes = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/drawer" component={Drawer} />
       </Switch>
     </BrowserRouter>
     //   </Provider>
